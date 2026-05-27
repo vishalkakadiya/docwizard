e6c4d22a8d5d9df74ec404b001e8b843d20ddb04
Vishal Kakadiya <vishalkakadiya123@gmail.com>
2026-05-27 10:09:34 +0530

    feat: project setup and config

---
+++ config.py
@@ -0,0 +1,8 @@
+OLLAMA_BASE_URL = "http://localhost:11434"
+EMBED_MODEL = "nomic-embed-text"
+LLM_MODEL = "llama3.2:1b"
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 50
+CHROMA_DB_PATH = "./chroma_db"
+COLLECTION_NAME = "docwizard"
+TOP_K = 5
